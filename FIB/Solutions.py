f048fbac33fb0d2bd132d835ee386466b31fd4f9
nhinguyen27 <148323774+NhiNguyenhcmus@users.noreply.github.com>
2024-10-27 21:49:53 +0700

    Create Solutions.py

---
+++ FIB/Solutions.py
@@ -0,0 +1,17 @@
+"""
+Problem: Rabbits and Recurrence Relations
+Version: Jan. 2, 2023
+"""
+
+n = int(input("Nhap so thang "))
+k = int(input("Nhap so cap moi lua "))
+if n <= 0:
+	print ("Nhap so thang lon hon 0!")
+elif n == 1 or n == 2:
+	print ("Tong so cap tho = 1")
+else:
+	num = [1 for j in range(0,n)]
+for i in range(2,n):	
+		num[i] = num[i-1] + k*num[i-2]
+print(num[-1])
+
